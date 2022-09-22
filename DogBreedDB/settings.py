70d562362aeaed9f12688b982e4c5c784dbef094
RickyGuo <rickyguo@Rickys-MacBook-Air.local>
2022-09-23 02:18:49 +1000

    deploy on heroku

--- DogBreedDB/settings.py
+++ DogBreedDB/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = "django-insecure-i&c%sh0vymc-2gt-!v+w=gw0@)3oop$+446+i&dfu22mwqnhlo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,6 +117,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'DogBreedDB/static/')]
 
+django_heroku.settings(locals(ALLOWED_HOSTS))
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn DogBreedDB.wsgi:application --log-file -
